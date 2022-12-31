b474be3de4746a9458a840e95ec2de07faf7c272
sturlagu <sturla8@gmail.com>
2022-12-31 10:43:58 +0100

    Advent of code day 9 - Refactoring and cleaning up code

--- day9/day9.groovy
+++ day9/day9.groovy
@@ -2,34 +2,32 @@ String input = new File('./input.txt').getText('UTF-8')
 String[] inputArray = input.split('\r\n')
 
 String[][] motionMap = new String[252][252];
+String[][] motionMapPart2 = new String[252][252];
 
-headPositionX = 0
-headPositionY = 0
-
-tailPositionX = 0
-tailPositionY = 0
+Knot head = new Knot(0)
+Knot tail = new Knot(-1)
 
 // Part 2
 ArrayList<Knot> knots = new ArrayList<>()
 (1..9).each {
     knots.add(new Knot(it))
 }
-Knot head = new Knot(0)
 
 inputArray.each { String line ->
     String[] command = line.split(' ')
     String direction = command[0]
     int numberOfMoves = Integer.parseInt(command[1])
     for (int i = 1; i <= numberOfMoves; i++) {
-        moveHead(direction)
-        moveTail(motionMap)
+        // Part 1
+        moveHead(direction, head)
+        moveTail(tail, head, motionMap)
 
-        moveHeadPart2(direction, head)
+        // Part 2
         knots.eachWithIndex { Knot knot, Integer index ->
             if(index == 0) {
-                moveTailPart2(knot, head, motionMap)
+                moveTail(knot, head, motionMapPart2)
             }else {
-                moveTailPart2(knot, knots.get(index - 1), motionMap)
+                moveTail(knot, knots.get(index - 1), motionMapPart2)
             }
         }
     }
@@ -44,114 +42,19 @@ int positionsVisitedCounter = 0
     }
 }
 
+int positionsVisitedCounter2 = 0
+(0..251).each { int i ->
+    (0..251).each { int y ->
+        if(motionMapPart2[i][y] == "#") {
+            positionsVisitedCounter2++
+        }
+    }
+}
+
 println("Task one - number of unique positions visited: ${positionsVisitedCounter}")
+println("Task two - number of unique positions visited: ${positionsVisitedCounter2}")
 
-// Task 2 - Guessed 10596, too high
-
-void moveHead(String direction) {
-    switch (direction) {
-        case 'L':
-            headPositionX--
-            break
-        case 'U':
-            headPositionY++
-            break
-        case 'R':
-            headPositionX++
-            break
-        case 'D':
-            headPositionY--
-            break
-    }
-}
-
-void moveTail(String[][] motionMap) {
-    // Diagonal
-    // Top right
-    if((headPositionX - tailPositionX) == 2 && (headPositionY - tailPositionY) == 1
-        || (headPositionX - tailPositionX) == 1 && (headPositionY - tailPositionY) == 2
-    ) {
-        if(headPositionX > tailPositionX) {
-            tailPositionX++
-        }else {
-            tailPositionX--
-        }
-        if(headPositionY > tailPositionY) {
-            tailPositionY++
-        }else {
-            tailPositionY--
-        }
-    // Bottom left
-    }else if((headPositionX - tailPositionX) == -2 && (headPositionY - tailPositionY) == -1
-            || (headPositionX - tailPositionX) == -1 && (headPositionY - tailPositionY) == -2
-    ) {
-        if(headPositionX > tailPositionX) {
-            tailPositionX++
-        }else {
-            tailPositionX--
-        }
-        if(headPositionY > tailPositionY) {
-            tailPositionY++
-        }else {
-            tailPositionY--
-        }
-    // Top left
-    }else if((headPositionX - tailPositionX) == -2 && (headPositionY - tailPositionY) == 1
-            || (headPositionX - tailPositionX) == -1 && (headPositionY - tailPositionY) == 2
-    ){
-        if(headPositionX > tailPositionX) {
-            tailPositionX++
-        }else {
-            tailPositionX--
-        }
-        if(headPositionY > tailPositionY) {
-            tailPositionY++
-        }else {
-            tailPositionY--
-        }
-    // Bottom right
-    }else if((headPositionX - tailPositionX) == 2 && (headPositionY - tailPositionY) == -1
-            || (headPositionX - tailPositionX) == 1 && (headPositionY - tailPositionY) == -2
-    ){
-        if(headPositionX > tailPositionX) {
-            tailPositionX++
-        }else {
-            tailPositionX--
-        }
-        if(headPositionY > tailPositionY) {
-            tailPositionY++
-        }else {
-            tailPositionY--
-        }
-    }else {
-        // X-direction
-        if((headPositionX - tailPositionX == 2)) {
-            tailPositionX = headPositionX - 1
-        }
-        if((headPositionX - tailPositionX == -2)) {
-            tailPositionX = headPositionX + 1
-        }
-        // Y-direction
-        if((headPositionY - tailPositionY == 2)) {
-            tailPositionY = headPositionY - 1
-        }
-        if((headPositionY - tailPositionY == -2)) {
-            tailPositionY = headPositionY + 1
-        }
-    }
-    //motionMap[convertNegativeIndex(tailPositionY)][convertNegativeIndex(tailPositionX)] = "#"
-}
-
-int convertNegativeIndex(int index) {
-    if (index < 0) {
-        index = -index * 2 - 1
-    }else {
-        index = index * 2
-    }
-    return index
-}
-
-void moveHeadPart2(String direction, Knot head) {
+void moveHead(String direction, Knot head) {
     switch (direction) {
         case 'L':
             head.x--
@@ -167,7 +70,7 @@ void moveHeadPart2(String direction, Knot head) {
             break
     }
 }
-void moveTailPart2(Knot knot, Knot knotInFront, String[][] motionMap) {
+void moveTail(Knot knot, Knot knotInFront, String[][] motionMap) {
     // Diagonal
     // Top right
     if((knotInFront.x - knot.x) == 2 && (knotInFront.y - knot.y) == 1
@@ -241,9 +144,23 @@ void moveTailPart2(Knot knot, Knot knotInFront, String[][] motionMap) {
             knot.y = knotInFront.y + 1
         }
     }
+    // Part 1
+    if(knot.index == -1){
+        motionMap[convertNegativeIndex(knot.y)][convertNegativeIndex(knot.x)] = "#"
+    }
+    // Part 2
     if(knot.index == 9){
         motionMap[convertNegativeIndex(knot.y)][convertNegativeIndex(knot.x)] = "#"
     }
+}
+
+int convertNegativeIndex(int index) {
+    if(index < 0) {
+        index = -index * 2 -1
+    }else {
+        index = index * 2
+    }
+    return index
 }
 
 class Knot {
